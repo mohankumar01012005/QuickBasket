2e423bf57151aae623fb0988ea905f23792c5245
mohankumar01012005 <kumar.mohan@kalvium.community>
2025-03-09 22:12:28 +0530

    implimented gluestack

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,32 @@
-import { View,Text } from "react-native"
+import { View, Text, FlatList } from "react-native";
+import products from "../assets/products.json";
+import { Button, ButtonText } from "@/components/ui/button";
 
-export default function HomeScreen(){
-  return(
-    <view>
-    <Text style={{fontSize:30}}>Home Screen</Text>
-    </view>
-  )
+type Product = {
+  id: number;  
+  name: string;
+  price?: number; 
+};
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+
+function ProductListItem({ product }: ProductListItemProps) {
+  return <Text style={{ fontSize: 30 }}>{product.name}</Text>;
+}
+
+// Home Screen Component
+export default function HomeScreen() {
+  return (
+    // <FlatList 
+    //   data={products}
+    //   keyExtractor={(item) => item.id.toString()} 
+    //   renderItem={({ item }) => <ProductListItem product={item} />}
+    // />
+    <Button>
+      <ButtonText>Done</ButtonText>
+    </Button>
+  );
 }
