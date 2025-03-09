272acc6af41df4a3510179d51d04c8197292056d
mohankumar01012005 <kumar.mohan@kalvium.community>
2025-03-09 19:45:01 +0530

    setup expo app and routes

---
+++ app/cart.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native"
+
+export default function CartScreen(){
+  return(
+    <view>
+    <Text style={{fontSize:30}}>Cart Screen</Text>
+    </view>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native"
+
+export default function HomeScreen(){
+  return(
+    <view>
+    <Text style={{fontSize:30}}>Home Screen</Text>
+    </view>
+  )
+}
